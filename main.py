89a371d2c6651e46ea11b2d2fd6a2cc06dad2e1c
Shivam Dubey <shivam@adsolut.in>
2025-05-23 03:13:44 +0530

    Neon DB connected successfully.

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from ai-email-assistance!")
+import uvicorn
+from fastapi import FastAPI
+from app.db.db import init_db
 
+app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    init_db()
+
+@app.get("/")
+def read_root():
+    return {"message": "Email AI Assistant is live!"}
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app, host="0.0.0.0", port=8000)
